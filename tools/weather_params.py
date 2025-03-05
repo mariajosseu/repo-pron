54e1a1c44881789db43086e5baf5cc9ec5823356
mariajosseu <maria.uribe@sansano.usm.cl>
2025-03-05 14:48:59 -0300

    arreglo api meteochile

--- tools/weather_params.py
+++ tools/weather_params.py
@@ -3,18 +3,18 @@ import requests
 from datetime import timedelta
 
 def fetch_and_process_weather_data_for_period(start_date, end_date):
+    print(f'end_date: {end_date}') 
     # Your credentials
     usuario = 'tomas.searle@coordinador.cl'
     token = '2fab7d3ae3c3175abf8aa5e7'
     codigo_nacional = '330020'
-
     # Create an empty DataFrame to store all the weather data
     all_weather_data = pd.DataFrame()
-
     # Loop over the period month-by-month
     current_date = start_date
     print(f'current date: {current_date}')
     while current_date <= end_date:
+        print(f'end date: {end_date}')
         year = current_date.strftime('%Y')  # Get the current year
         month = current_date.strftime('%m')  # Get the current month
 
@@ -88,6 +88,63 @@ def fetch_and_process_weather_data_for_period(start_date, end_date):
 
     return all_weather_data  # Return all the weather data for the period
 
+def fetch(start_date, end_date, usuario, token, codigo_nacional):
+    # Create an empty DataFrame to store all the weather data
+    all_weather_data = pd.DataFrame()
+    # Loop over the period month-by-month
+    current_date = start_date
+    print(f'current date: {current_date}')
+    while current_date <= end_date:
+        print(f'end date: {end_date}')
+        year = current_date.strftime('%Y')  # Get the current year
+        month = current_date.strftime('%m')  # Get the current month
+
+        # API endpoint
+        url = f'https://climatologia.meteochile.gob.cl/application/servicios/getDatosRecientesEma/{codigo_nacional}/{year}/{month}'
+        print(url)
+        # Parameters for the API request
+        params = {
+            'usuario': usuario,
+            'token': token
+        }
+
+        # Make the request with SSL verification disabled
+        response = requests.get(url, params=params, verify=False)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            if 'datosEstaciones' in data and 'datos' in data['datosEstaciones']:
+                weather_records = data['datosEstaciones']['datos']
+                # Create a DataFrame from the weather records
+                api_weather_df = pd.DataFrame(weather_records)
+                # Convert 'momento' to a datetime object
+                api_weather_df['momento'] = pd.to_datetime(api_weather_df['momento'])
+                # Set the 'momento' column to UTC timezone (since the weather data is in UTC)
+                api_weather_df['momento'] = api_weather_df['momento'].dt.tz_localize('UTC')
+                # Convert the 'momento' column from UTC to Chilean local time
+                api_weather_df['momento'] = api_weather_df['momento'].dt.tz_convert('America/Santiago')
+                # Remove timezone information and keep only the datetime
+                api_weather_df['momento'] = api_weather_df['momento'].dt.tz_localize(None)
+                # Clean up and convert API fields to match historical DataFrame fields
+                api_weather_df['Temperatura'] = api_weather_df['temperatura'].str.replace(' °C', '').astype(float)
+                # Select relevant columns for further processing
+                relevant_columns = ['momento', 'Temperatura']
+                api_weather_df = api_weather_df[relevant_columns]
+                # Set 'momento' as the index
+                api_weather_df.set_index('momento', inplace=True)
+                # Resample the data to hourly frequency (taking mean for continuous data)
+                hourly_api_weather_df = api_weather_df.resample('H').mean()
+                # Append to all_weather_data
+                all_weather_data = pd.concat([all_weather_data, hourly_api_weather_df])
+            else:
+                print(f"Unexpected response structure: {data}")
+        else:
+            print(f"Failed to fetch data from API. Status code: {response.status_code}")
+        # Move to the next month
+        current_date += timedelta(days=32)
+        current_date = current_date.replace(day=1)  # Reset to the first day of the next month
+    return all_weather_data  # Return all the weather data for the period
 
 def append_new_api_data(merged_df, api_weather_df):
     if api_weather_df.empty:
@@ -96,9 +153,7 @@ def append_new_api_data(merged_df, api_weather_df):
 
     # Merge the new weather data with the existing merged data on 'momento'
     merged_df = pd.merge(merged_df, api_weather_df, on='momento', how='outer', suffixes=('', '_new'))
-
     # Now, for each weather-related column, update missing values in the original with the new data
-    #weather_columns = ['Temperatura', 'PuntoRocio', 'Humedad', 'IndiceUVB', 'dd_Valor', 'ff_Valor', 'RRR6_Valor']
     weather_columns = ['Temperatura']
     for col in weather_columns:
         if f"{col}_new" in merged_df.columns:
